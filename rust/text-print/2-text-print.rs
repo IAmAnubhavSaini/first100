0ebe081a5c83406f6d6e491b087b2be0bf7abde3
f0c1s <iamanubhavsaini+git@gmail.com>
2023-10-06 00:24:03 +0530

    Create 2-text-print.rs

---
+++ rust/text-print/2-text-print.rs
@@ -0,0 +1,14 @@
+use std::fmt::Display;
+
+
+fn info<T:Display>(input: T) {
+    println!("{}", input);
+}
+
+fn main() {
+    let x = String::from("what");
+    info(x);
+    info("abc");
+    info("abc".to_string());
+    info("abc".to_ascii_lowercase());
+}
